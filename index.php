305cf234134d4a5d01d2d83c2d4cb21d38e89fc4
Myro237 <myrotchamou00@gmail.com>
2023-12-05 17:22:30 +0100

    Ajout de l'index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $servername = 'mysql-myro.alwaysdata.net';
+    $username = 'myro';
+    $password = 'Kemayou7';
+    //On établit la connexion 
+    $con = new PDO("mysql:host=$servername;dbname=myro_garage", $username, $password);
+    if($con){
+        $reponse = $con->prepare('SELECT * FROM admin');
+        $reponse->execute();
+        $userData = [];
+            while($data = $reponse->fetch()){
+                $userData [] = $data;
+            }
+            echo json_encode($userData);
+    }else{
+        echo 'echec de connexion';
+    }
+?> 
