fe3ab10b66e55692dae0ce36df255494084e45e1
Myro237 <115348670+Myro237@users.noreply.github.com>
2023-12-05 18:06:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,13 +16,3 @@
         echo 'echec de connexion';
     }
 ?> 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Mon essaie</h1>
-</body>
-</html>
