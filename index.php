cd3df5992deb4991a319091188b916f24b4ff056
Myro237 <115348670+Myro237@users.noreply.github.com>
2023-12-05 17:56:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
     $servername = 'mysql-myro.alwaysdata.net';
     $username = 'myro';
     $password = 'Kemayou7';
@@ -17,3 +16,14 @@
         echo 'echec de connexion';
     }
 ?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Mon essaie</h1>
+</body>
+</html>
